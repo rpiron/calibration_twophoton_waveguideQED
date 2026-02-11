8fa244459089b1d83e7a7688ba23688f6a9191c9
Romain perso laptop <romain-piron@live.fr>
2026-02-11 14:09:40 +0100

    Try new renormalization relations

--- experiments/coincidence_vs_bandwith.py
+++ experiments/coincidence_vs_bandwith.py
@@ -14,15 +14,15 @@ from src.bare_param import get_bare_param
 
 pi = np.pi
 
-def run_antiHOM_vs_bandwith(omega_q, omega_ref, lbda_tab, index_omega_q = 0, index_experiment = 0, 
-                            store_results:bool=True, progress:bool=True):
+def run_coincidence_vs_bandwith(omega_q, ir_tab, uv_tab, index_omega_q = 0, index_experiment = 0, 
+                                sym_variables:bool=True, store_results:bool=True, progress:bool=True):
     """
-    Computes the antiHOM probability at omega_q = omega_A - Gamma/2 for different bandwith
+    Computes the coincidence probability at omega_q for different frequency windows
     
     Parameters:
     omega_q (float) : Photons incoming frequency
-    omega_ref (float) : Center of the frequency window
-    lbda_tab (np.array) : Array of different bandwith, defining several frequency window to test
+    ir_tab (np.array) : Array of different ir cutoffs
+    uv_tab (np.array) : Array of different uv cutoffs
 
     Returns:
     antiHOM_proba_tab (np.array) : Array of measured anti HOM probabilities for each frequency window
@@ -32,20 +32,20 @@ def run_antiHOM_vs_bandwith(omega_q, omega_ref, lbda_tab, index_omega_q = 0, ind
 
     omega_A = 10*pi
     Gamma = 5*pi
-    antiHOM_proba_tab = np.zeros(len(lbda_tab))
-
-    for i in tqdm(range(len(lbda_tab)), disable=not progress):
+    coincidence_tab = np.zeros(len(ir_tab))
+    for i in tqdm(range(len(ir_tab)), disable=not progress):
 
         #Frequency window
-        cutoffs = {'ir_cutoff': omega_ref - lbda_tab[i] , 'uv_cutoff': omega_ref + lbda_tab[i]}
+        cutoffs = {'ir_cutoff': ir_tab[i] , 'uv_cutoff': uv_tab[i]}
 
         #Sanity check
-        if omega_q < cutoffs['ir_cutoff']:
+        if omega_q < cutoffs['ir_cutoff'] or omega_q > cutoffs['uv_cutoff'] :
             print("WARNING : The photon frequency is not included in the frequency window. Returning NaN")
-            antiHOM_proba_tab[i] = np.nan
+            coincidence_tab[i] = np.nan
+        
         else:
             #Bare parameters
-            omega_0, gamma = get_bare_param(omega_A, Gamma, omega_ref, lbda_tab[i])
+            omega_0, gamma = get_bare_param(omega_A, Gamma, ir_tab[i], uv_tab[i])
 
             #Parameters of the simulation
             L = 50
@@ -73,16 +73,21 @@ def run_antiHOM_vs_bandwith(omega_q, omega_ref, lbda_tab, index_omega_q = 0, ind
             P12_final = np.sum(np.abs(experiment.c_array[:n_modes, n_modes:])**2)
             P21_final = np.sum(np.abs(experiment.c_array[n_modes:, :n_modes])**2)
 
-            antiHOM_proba_tab[i] = P12_final + P21_final
+            coincidence_tab[i] = P12_final + P21_final
 
             del experiment
 
     if store_results:
-        data_to_save = {'lbda_tab': lbda_tab, 'antiHOM_proba_tab': antiHOM_proba_tab}
+        data_to_save = {'ir_tab': ir_tab, 'uv_tab': uv_tab, 'coincidence_tab': coincidence_tab}
         df = pd.DataFrame(data_to_save)
+
         if index_experiment:
-            df.to_csv(project_root / 'results' / 'csv_files' /f'antiHOM_vs_bandwith_{index_omega_q}_{index_experiment}.csv', index=False)
+            if sym_variables:
+                df.to_csv(project_root / 'results' / 'csv_files' / 'sym_variables' /f'coincidence_vs_bandwith_{index_omega_q}_{index_experiment}.csv', index=False)
+            else:
+                df.to_csv(project_root / 'results' / 'csv_files' / 'iruv_variables' /f'coincidence_vs_bandwith_{index_omega_q}_{index_experiment}.csv', index=False)
+        
         else:
-            df.to_csv(project_root / 'results' / 'csv_files' /'antiHOM_vs_bandwith.csv', index=False)
+            df.to_csv(project_root / 'results' / 'csv_files' /'coincidence_vs_bandwith.csv', index=False)
     
-    return lbda_tab, antiHOM_proba_tab
+    return ir_tab, uv_tab, coincidence_tab
--- src/bare_param.py
+++ src/bare_param.py
@@ -1,6 +1,7 @@
 import numpy as np
+pi = np.pi
 
-def get_bare_param(omega_A, Gamma, omega_ref, lbda, precision=1e-4):
+def get_bare_param(omega_A, Gamma, ir, uv, precision=1e-4):
     """
     Computes the bare parameters by inverting the renormalization relations
     (omega_0, gamma) = F(omega_A, Gamma, omega_ref, lbda)
@@ -8,8 +9,8 @@ def get_bare_param(omega_A, Gamma, omega_ref, lbda, precision=1e-4):
     Parameters:
     omega_A : physical transition frequency of the TLS
     Gamma : physical decay rate of the TLS
-    omega_ref : central fequency of the numerical frequency window
-    lbda : bandwith of the frequency window, defined as [IR, UV] = [omega_ref - lbda, omega_ref + lbda] 
+    ir: infrared cutoff of the spectral density
+    uv: ultraviolet cutoff of the spectral density
     precision : precision parameter for the dichotomy research
 
     Returns:
@@ -17,28 +18,67 @@ def get_bare_param(omega_A, Gamma, omega_ref, lbda, precision=1e-4):
     gamma : bare decay rate to parameterize the Hamiltonian
     """
     
-    ## Obtain the bare frequency
+    ##Express the bare decay as a function of omega_0
+    def adjust_gamma(x):
+        return Gamma / (1 - Gamma/(2*pi) * ((ir + omega_A - 2*x)/(ir - x)**2 - (uv + omega_A - 2*x)/(uv - x)**2))
 
-    #deviation from the central frequency
-    dwA = omega_A - omega_ref
-
-    #Initial guess : omega0 in [omega_ref - lbda, omega_ref + lbda] (equivalently, dw0 in [-lbda, lbda])
-    dw0_inf = -1*lbda 
-    dw0_sup = lbda
+    ## Obtain the bare frequenc by a dichtomy search
+    w0_inf = 1.01*ir
+    w0_sup = 0.99*uv
     residual_diff = np.inf
 
     while np.abs(residual_diff) > precision: #research by dichotomy
 
-        dw0_guess = 0.5*(dw0_inf + dw0_sup)
-        residual_diff = dwA - dw0_guess + Gamma /(2*np.pi) * np.log((lbda-dw0_guess)/(lbda+dw0_guess)) 
+        w0_guess = 0.5*(w0_inf + w0_sup)
+        gamma_guess = adjust_gamma(w0_guess)
+        residual_diff = gamma_guess/(4*pi) * (1/(ir - w0_guess)**2 - 1/(uv - w0_guess)**2) * (Gamma**2 / 4 - (w0_guess - omega_A)**2) \
+                        - (1 + gamma_guess/(2*pi) * (1/(ir - w0_guess) - 1/(uv - w0_guess))) * (w0_guess - omega_A) \
+                        + gamma_guess / (2*pi) * np.log((uv - w0_guess)/(w0_guess - ir))
+
         if residual_diff > 0:
-            dw0_inf = dw0_guess
+            w0_inf = w0_guess
         else:
-            dw0_sup = dw0_guess
+            w0_sup = w0_guess
 
-    omega_0 = omega_ref + dw0_guess
+    #Last round needed to update gamma
+    gamma_guess = adjust_gamma(w0_guess)
 
-    ## Obtain the bare decay rate
-    gamma = 1/(1/Gamma + lbda / (np.pi*(lbda**2 - dw0_guess**2)))
+    return w0_guess, gamma_guess
 
-    return omega_0, gamma
+
+def get_bare_param_first_order(omega_A, Gamma, ir, uv, precision=1e-4):
+    """
+    Computes the bare parameters by inverting the renormalization relations
+    (omega_0, gamma) = F(omega_A, Gamma, omega_ref, lbda)
+    
+    Parameters:
+    omega_A : physical transition frequency of the TLS
+    Gamma : physical decay rate of the TLS
+    ir: infrared cutoff of the spectral density
+    uv: ultraviolet cutoff of the spectral density
+    precision : precision parameter for the dichotomy research
+
+    Returns:
+    omega_0 : bare frequency to parameterize the Hamiltonian
+    gamma : bare decay rate to parameterize the Hamiltonian
+    """
+    
+    ## Obtain the bare frequenc by a dichtomy search
+    w0_inf = 1.01*ir
+    w0_sup = 0.99*uv
+    residual_diff = np.inf
+
+    while np.abs(residual_diff) > precision: #research by dichotomy
+
+        w0_guess = 0.5*(w0_inf + w0_sup)
+        residual_diff = omega_A - w0_guess + Gamma /(2*pi) * np.log((uv - w0_guess) / (w0_guess - ir))
+
+        if residual_diff > 0:
+            w0_inf = w0_guess
+        else:
+            w0_sup = w0_guess
+
+    #Deduce gamma accordingly
+    gamma = Gamma / (1 - Gamma/(2*pi)*(1/(ir - w0_guess) - 1/(uv - w0_guess)))
+    
+    return w0_guess, gamma
